8baa9ca630955d13b01624d9a0cbb6901c2c1386
Raelinda Shoates <redfire809@gmail.com>
2021-03-31 14:47:03 -0400

    added a function that plays a round to determine who won and who lost

--- main.js
+++ main.js
@@ -5,4 +5,23 @@ function computerPlay() {
   return computerChoice;
 }
 
-
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+     if (playerChoice === computerSelection) {
+         return "it's a tie!";
+     } else if (playerChoice === "rock" && computerSelection === "paper") {
+         return "You Lose!";
+     }  else if (playerChoice === "paper" && computerSelection === "rock") {
+         return "You Won!";
+     } else if (playerChoice === "paper" && computerSelection === "scissors") {
+         return "You Lose!";
+     } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        return "You Win!";
+    } else if (playerChoice === "rock" && computerSelection === "scissors") {
+        return "You Win!";
+    } else if (playerChoice === "scissors" && computerSelection === "rock") {
+        return "You Lose!";
+    } else {
+       return "Oops, something went wrong!";
+     }
+}
